c623a7de1707254edfab75e3cb10bd3abe60e428
ntancardoso <niny0@msn.com>
2024-11-06 23:42:41 +0800

    using chatollama
    - using ragchain
    - without ragchain

--- lc_ollama_ragchain.py
+++ lc_ollama_ragchain.py
@@ -1,9 +1,10 @@
+from langchain_ollama import ChatOllama
 from langchain_ollama import OllamaEmbeddings
 from langchain_community.vectorstores import Chroma, SKLearnVectorStore
 from langchain_core.prompts import PromptTemplate
-from langchain_ollama import ChatOllama
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
+
 
 
 documents = [
@@ -24,12 +25,6 @@ retriever = db.as_retriever(
     search_type="similarity",
     search_kwargs={'k': 1}
 )
-
-
-# result = retriever.invoke("Who livs in a pineaple under the sea?")
-# print(result)
-
-
 template = """Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
 Use three sentences maximum and keep the answer as concise as possible.
@@ -44,17 +39,21 @@ custom_rag_prompt = PromptTemplate.from_template(template)
 # print(custom_rag_prompt)
 
 question = "Who livs in a pineaple under the sea?"
-retriever_context = retriever.invoke(question)
+context = retriever.invoke(question)
+# print(context)
 
-augmented_query = custom_rag_prompt.format(context=retriever_context, question=question)
-
-context_content = retriever_context[0].page_content if retriever_context else "No context found."
-
-augmented_query = custom_rag_prompt.format(context=context_content, question=question)
+augmented_query = custom_rag_prompt.format(context=context, question=question)
+# print("Augmented Query:")
+# print(augmented_query)
 
 llm = ChatOllama(model="llama3.1")
-llm_output = llm.invoke(augmented_query)
 
-final_output = StrOutputParser().invoke(llm_output)
+rag_chain = (
+    {"context": retriever, "question": RunnablePassthrough()}
+    | custom_rag_prompt
+    | llm
+    | StrOutputParser()
+)
 
-print(final_output)
+response = rag_chain.invoke(question)
+print(response)
